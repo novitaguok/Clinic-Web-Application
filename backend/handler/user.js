d5a8524742533a64981c5866e9b17514d0fbaec0
Alif Naufal <alifnaufalyasin@gmail.com>
2020-04-06 13:18:53 +0700

    Update

--- backend/handler/user.js
+++ backend/handler/user.js
@@ -2,9 +2,9 @@ function regisUser(req,res) {
   const payload = req.body
   console.log(payload)
   // res.send(payload)
-  res.render('home', {
-    layout: 'main'
-  })
+  // res.render('home', {
+  //   layout: 'main'
+  // })
 }
 
 module.exports = {
